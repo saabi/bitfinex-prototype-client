6004d553ee115bbfab130c72ed233d372a2001d2
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2018-02-01 19:35:48 -0300

    Created multiple stores for the data types coming from a crypto exchange.

--- src/client/store.ts
+++ src/client/store.ts
@@ -1,42 +1,58 @@
-import { createStore, Store as UnduxStore} from 'undux';
+import { createStore, Store as UnduxStore } from 'undux';
 
 import * as BF from './bitfinex/types';
 
+type OrderDirection = 'up' | 'down' | 'unsorted';
+type TickerOrder = 'symbol' | 'last' | '24hr' | 'volume';
+
 export namespace Exchange {
 
-    type AppModel = {
+    type TickerModel = {
         tickers: BF.Ticks;
-        groups: {[name:string]: BF.SymbolDetail[]};
-        tickerOrderColumn: TickerOrder;
-        tickerOrderDirection: OrderDirection;
-    }
-    export type AppStore = UnduxStore<AppModel>;
-    export const AppStore = createStore<AppModel>({
-        tickers: {},
-        groups: {},
-        tickerOrderColumn: 'symbol',
-        tickerOrderDirection: 'unsorted'
-    });
-
-
-    export type AppProps = {
-        store: AppStore
-    }
-
-    type OrderDirection = 'up' | 'down' | 'unsorted';
-
-    type TickerOrder = 'symbol' | 'last' | '24hr' | 'volume';
-
-    export type TickerProps = {
-        tickers: BF.Ticks;
-        groups: {[name:string]: BF.SymbolDetail[]};
+        groups: { [name: string]: BF.SymbolDetail[] };
         orderColumn: TickerOrder;
         orderDirection: OrderDirection;
     }
-
-    export type BookProps = {
+    export type TickerStore = UnduxStore<TickerModel>;
+    export const TickerStore = createStore<TickerModel>({
+        tickers: {},
+        groups: {},
+        orderColumn: 'symbol',
+        orderDirection: 'unsorted'
+    });
+    export type TickerProps = {
+        store: TickerStore;
     }
 
+
+    type BookModel = {
+    }
+    export type BookStore = UnduxStore<BookModel>;
+    export const BookStore = createStore<BookModel>({
+    });
+    export type BookProps = {
+        store: BookStore;
+    }
+
+
+    type TradesModel = {
+    }
+    export type TradesStore = UnduxStore<TradesModel>;
+    export const TradesStore = createStore<TradesModel>({
+    });
     export type TradesProps = {
+        store: TradesStore;
+    }
+
+
+    type AppModel = {
+        currentSymbol: string;
+    }
+    export type AppStore = UnduxStore<AppModel>;
+    export const AppStore = createStore<AppModel>({
+        currentSymbol: 'BTCUSD'
+    });
+    export type AppProps = {
+        store: AppStore;
     }
 }
