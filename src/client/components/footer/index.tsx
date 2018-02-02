45e4bcdc08eb65f550d39b5e46327781ad7475fd
Saabi <ushiferreyra@gmail.com>
2018-02-02 00:42:46 -0300

    Added header and footer components.

---
+++ src/client/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Footer extends React.Component<{}> {
+    render() {
+        return (
+            <div id='footer'>
+                <h2>Footer</h2>
+            </div>
+        )
+    }
+}
---
+++ src/client/components/header/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Header extends React.Component<{}> {
+    render() {
+        return (
+            <div id='header'>
+                <h2>Header</h2>
+            </div>
+        )
+    }
+}
