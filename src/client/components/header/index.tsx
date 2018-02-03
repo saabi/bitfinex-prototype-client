1bfca7f97d1008d3b66046d8bcbf70d58f1d78d6
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2018-02-02 23:04:57 -0300

    Added new properties to the Header component.

--- src/client/components/header/index.tsx
+++ src/client/components/header/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 
-export class Header extends React.Component<{}> {
+export type HeaderProps = {
+    isConnected: boolean;
+    currentSymbol: string | null;
+}
+
+export class Header extends React.Component<HeaderProps> {
     render() {
+        let symbol = this.props.currentSymbol || 'no symbol selected';
         return (
             <div id='header'>
                 <h1>Bitfinex</h1>
                 <h2>Prototype Client</h2>
+                <p id="currentSymbol">{symbol}</p>
+                <p id='connectionStatus'>{this.props.isConnected ? 'Connected' : 'Disconnected'}</p>
             </div>
         )
     }
