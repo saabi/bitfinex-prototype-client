f5cab4170d637a8f6b2bb3d17cf21e58844454d6
Saabi <ushiferreyra@gmail.com>
2018-02-03 19:49:37 -0300

    Implemented shouldComponentUpdate on Trades widget.

--- src/client/components/trades/index.tsx
+++ src/client/components/trades/index.tsx
@@ -1,5 +1,25 @@
 import * as React from 'react';
 import * as Exchange from '../../stores';
+import { TradeTick } from '../../bitfinex/types';
+
+interface RowProps {
+    trade: TradeTick;
+}
+export class TradeRow extends React.Component<RowProps> {
+    shouldComponentUpdate(nextProps: RowProps) {
+        return nextProps.trade !== this.props.trade;
+    }
+    render() {
+        let trade = this.props.trade;
+        return (
+            <tr className={trade.amount>0? 'bought' : 'sold'}>
+                <td>{new Date(trade.mts).toLocaleTimeString()}</td>
+                <td>{trade.price.toPrecision(5)}</td>
+                <td>{Math.abs(trade.amount).toFixed(4)}</td>
+            </tr>
+        )
+    }
+}
 
 export class Trades extends React.Component<Exchange.TradesProps> {
     render() {
@@ -7,17 +27,6 @@ export class Trades extends React.Component<Exchange.TradesProps> {
         symbol = symbol ? ' - ' + symbol : '';
 
         let trades = this.props.store.get('trades');
-
-        let tradeRows = trades.map((t) => {
-            return (
-                <tr key={t.id} className={t.amount>0? 'bought' : 'sold'}>
-                    <td>{new Date(t.mts).toLocaleTimeString()}</td>
-                    <td>{t.price.toPrecision(5)}</td>
-                    <td>{Math.abs(t.amount).toFixed(4)}</td>
-                </tr>
-            )
-        });
-
         return (
             <div id='trades' className='widget'>
                 <h3>Trades{symbol}</h3>
@@ -30,7 +39,7 @@ export class Trades extends React.Component<Exchange.TradesProps> {
                         </tr>
                     </thead>
                     <tbody>
-                        {tradeRows}
+                        { trades.map( t => <TradeRow key={t.id} trade={t} /> ) }
                     </tbody>
                 </table>
             </div>
