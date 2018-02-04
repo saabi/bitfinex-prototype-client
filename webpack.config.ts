ec40e86d28e0fc9f19f4c1a9d6fe8aebe4200989
Saabi <ushiferreyra@gmail.com>
2018-02-04 20:17:38 -0300

    Added production and development build configurations.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
 import * as webpack from "webpack";
 import * as path from "path";
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 import * as express from 'express';
 import fetch from 'node-fetch';
@@ -19,44 +19,85 @@ function setupBitfinexProxy(app: express.Application) {
     });
 }
 
-const config: webpack.Configuration = {
-    entry: [
-        'react-hot-loader/patch',
-        "./src/client/main.tsx"
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ["react-hot-loader/webpack", "ts-loader"],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    plugins: [
-        //new UglifyJSPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'public/js/dist'),
-        publicPath: '/js/dist/'
-    },
-    devtool: 'source-map',
-    devServer: {
-        hotOnly: true,
-        inline: true,
-        contentBase: 'public/',
-        before: setupBitfinexProxy,
-        allowedHosts: ['.lan']
+namespace Configurations {
+    export const production: webpack.Configuration = {
+        entry: "./src/client/main.tsx",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: ["ts-loader"],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        plugins: [
+            new UglifyJSPlugin()
+        ],
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'public/js/dist'),
+            publicPath: '/js/dist/'
+        },
+        devServer: {
+            contentBase: 'public/',
+            before: setupBitfinexProxy,
+            disableHostCheck: true
+        }
     }
+    export const development: webpack.Configuration = {
+        entry: [
+            'react-hot-loader/patch',
+            "./src/client/main.tsx"
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: ["react-hot-loader/webpack", "ts-loader"],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        plugins: [
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()],
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'public/js/dist'),
+            publicPath: '/js/dist/'
+        },
+        devtool: 'source-map',
+        devServer: {
+            hotOnly: true,
+            inline: true,
+            contentBase: 'public/',
+            before: setupBitfinexProxy,
+            allowedHosts: ['.lan']
+        }
+    }    
 }
 
-export default config;
+export default function config(env: any, argv: any) {
+    if (!env)
+        env = 'production';
+    if (!(env in Configurations)) {
+        console.log('No configuration with that name.')
+        return;
+    }
+    return Configurations[env];
+};
