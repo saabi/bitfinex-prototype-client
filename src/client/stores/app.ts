a76ad611fc14d7310a8df8859ee6ae72eeabc4fe
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2018-02-06 19:01:40 -0300

    Added reconnecting state and testDisconnection action to App store.

--- src/client/stores/app.ts
+++ src/client/stores/app.ts
@@ -3,16 +3,20 @@ import { createStore, Store as UnduxStore } from 'undux';
 
 type AppModel = {
     isConnected: boolean;
+    isConnecting: boolean;
     isRateLimited: boolean;
     currentSymbol: string | null;
+    testDisconnection: boolean;
 }
 
 export type AppStore = UnduxStore<AppModel>;
 
 export const AppStore = createStore<AppModel>({
     isConnected: false,
+    isConnecting: false,
     isRateLimited: false,
-    currentSymbol: null
+    currentSymbol: null,
+    testDisconnection: false
 });
 
 export type AppProps = {
